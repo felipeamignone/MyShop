db67cbe7544519fc9da39a8f2931598bf6e42399
Felipe Mignone <dev.felipemignone@gmail.com>
2022-07-12 20:24:04 -0300

    test: add tests to icons

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -2,7 +2,7 @@ import Header from '../Header';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {render, screen} from "@testing-library/react";
 
-const renderHeader = () => render(<Router><Header/></Router>)
+const renderHeader = () => render(<Router><Header/></Router>);
 describe("Header", () => {
     it('render Header title', () => {
         renderHeader();
@@ -12,6 +12,16 @@ describe("Header", () => {
         });
         expect(title).toBeInTheDocument();
     });
-    it.todo('render Home icon');
-    it.todo('render Cart icon');
+    it('render Home icon', () => {
+        renderHeader();
+
+        const homeIcon = screen.getByTestId(/homeicon/i);
+        expect(homeIcon).toBeInTheDocument();
+    });
+    it('render Cart icon', () => {
+        renderHeader();
+
+        const cartIcon = screen.getByTestId(/shoppingcarticon/i);
+        expect(cartIcon).toBeInTheDocument();
+    });
 })
