2c5d3b2ece6732437fb15da8401170bd72bb0dd6
Felipe Mignone <dev.felipemignone@gmail.com>
2022-12-14 01:22:26 -0300

    feat: integrate with fakestore api

--- src/contexts/catalog/useCatalog.tsx
+++ src/contexts/catalog/useCatalog.tsx
@@ -1,7 +1,14 @@
-import {useState,} from "react";
-import {IProduct} from "../../pages/Catalog/types";
-import {ProductMock} from "../../mocks/ProductMock";
+import { useCallback, useEffect, useState, } from "react";
+import { IProduct } from "../../pages/Catalog/types";
 
+interface IFakeStoreProduct {
+    id: number,
+    title: string,
+    price: string,
+    category: string,
+    description: string,
+    image: string
+}
 
 interface ICatalogState {
     madeInitialFetch: boolean;
@@ -10,14 +17,47 @@ interface ICatalogState {
 
 const initialState: ICatalogState = {
     madeInitialFetch: false,
-    products: ProductMock
+    products: [],
 }
 
 export const useCatalog = () => {
     const [state, setState] = useState<ICatalogState>(initialState);
 
-    const handleState = (changes: Partial<ICatalogState>) => setState({...state, ...changes});
+    const handleState = useCallback((changes: Partial<ICatalogState>) =>
+        setState((oldState) => ({ ...oldState, ...changes })), []);
 
+    const fetchCatalog = useCallback(async () => {
+        try {
+            const res: IFakeStoreProduct[] = await fetch('https://fakestoreapi.com/products')
+                .then(res => res.json());
+
+            let newProductsArray: IProduct[] = [];
+
+            res.forEach(product => {
+                const newProduct: IProduct = {
+                    id: product.id,
+                    name: product.title,
+                    price: Number(product.price),
+                    imgSrc: product.image,
+                    availableAmount: Math.floor(Math.random() * 30) + 1
+                }
+
+                newProductsArray.push(newProduct);
+            })
+
+            handleState({ products: [...newProductsArray] })
+        }
+        catch (error) { console.log(error) }
+        finally {
+            handleState({ madeInitialFetch: true })
+        }
+    }, [handleState])
+
+    useEffect(() => {
+        if (!state.madeInitialFetch) {
+            fetchCatalog();
+        }
+    }, [fetchCatalog, state.madeInitialFetch])
 
     return ({
         state,
